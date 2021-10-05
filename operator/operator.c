1f7f156601c283612592eb3e8caa9c9d7359042c
langmoyu <91683249+langmoyu@users.noreply.github.com>
2021-10-05 19:31:08 +0800

    Add files via upload

---
+++ operator/operator.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+{
+	//Arithmetic operator
+	//1."+","-","*"
+	/*
+	int a = 10;
+	int b = 20;
+	int c = 10 + 20;
+	printf("%d\t", c);
+	printf("%d\n", a + b + c);
+
+	char a = 10;
+	char b = 20;
+	char c = a + b;
+	printf("%d\t", c);
+	printf("%d\n", a + b + c);
+
+	unsigned char a = -10;
+	unsigned char b = -20;
+	unsigned char c = a + b;
+	printf("%d\t", c);
+	printf("%d\n", a + b + c);
+
+
+	short a = 5;
+	short b = 6;
+	short c = a + b;
+	printf("%d\t", c);
+	printf("%d\n", a + b + c);
+
+
+
+	unsigned short a = -5;
+	unsigned short b = -6;
+	unsigned short c = a + b;
+	printf("%d\t", c);         	
+	printf("%d\n", a + b + c); 
+
+	unsigned int a = 5;
+        unsigned int b = 6;
+        unsigned int c = a + b;
+        printf("%d\t", c);
+        printf("%d\n", a + b + c);
+
+	long int a = 1000;
+	long int b = 3800;
+	long int c = a + b;
+	printf("%ld\t", c);         	
+	printf("%ld\n", a + b + c); 	
+      	
+	long long int a = 1000;              	
+	long long int b = 3800;              	
+	long long int c = a + b;             	
+        printf("%lld\t", c);         	
+        printf("%lld\n", a + b + c); 	
+
+
+	printf("%ld\n", sizeof(long long int));
+
+	printf("%ld\n", sizeof(long int));
+
+
+	float a = 3.14f;
+	float b = 4.13f;
+	printf("%f\n", a + b);       
+
+
+	float a = 3.14f;
+	float b = 4.13f;
+	printf("%f\n", a + b);      
+
+	double a = 3.14;
+	double b = 4.13;
+	printf("%f\n", a + b);   
+
+	*/
+
+
+	return 0;
+}
